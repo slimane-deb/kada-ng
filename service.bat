e2e04c692e2158489d94d80bc5b6943f5c258b1a
s-deb <s-deb@enageo.com>
2020-02-08 13:24:39 +0100

    add nssm

--- service.bat
+++ service.bat
@@ -1,4 +1,6 @@
-REM @echo off
-START "Backend" /B "back\kada4j.exe"
+REM WARNING: use NSSM to install backend as service
+@echo off
+REM START "Backend" /B "back\kada4j.exe"
 REM SC CREATE Backend Displayname= "Backend-AutoEcole" binpath= "%LOCALAPPDATA%\Programs\kada-ng\back\kada4j.exe" start= auto
-REM pause
+start "nssm\win64\nssm.exe" install Backend kada4j.exe
+pause
